702dad1ee555b0edd6a4b3cd3a3534eb71ff8ecc
venkat669 <venkatramanvaithee@gmail.com>
2024-07-13 23:04:15 +0530

    added comments to code

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+ /*
+flutter is made of widgits
+each element that we see on screen is a widgit
+the view of screen is dependent on the ordering of these widgits
+it follows a tree structre
 
+ */ 
 void main() {
   runApp(MaterialApp(
     home: Homepage(),
@@ -15,16 +21,16 @@ class Homepage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text("Hello Guy"),
-         backgroundColor: Colors.blue,
+         backgroundColor: Colors.blue,  // background color for app bar title
       ),
       body: Container(
         child: Center(
           child: Text("Hello User",
-           style: TextStyle(backgroundColor: Colors.grey , 
-           fontStyle: FontStyle.italic, 
-           fontFamily:'Roboto',
-            color: Color.fromRGBO(254, 36, 7, 0.962),
-            fontSize: 30),),
+           style: TextStyle(backgroundColor: Colors.grey , // gives grey background for the text
+           fontStyle: FontStyle.italic, // font is change to itallic
+           fontFamily:'San Fransisco', 
+            color: Color.fromRGBO(254, 36, 7, 0.962), // gives color for the text
+            fontSize: 30),), // size of text
            
         ),
         
