5a0c23ea9206859b046ca5d6194fc075054ec015
venkat669 <venkatramanvaithee@gmail.com>
2024-07-13 22:42:37 +0530

    learned how to create appBar, how to allocate a text and change its size , color , font ,

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,33 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MaterialApp(
+    home: Homepage(),
+    theme: ThemeData(
+      primarySwatch: Colors.purple,
+    ),
+  ));
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Hello Guy"),
+         backgroundColor: Colors.blue,
+      ),
+      body: Container(
+        child: Center(
+          child: Text("Hello User",
+           style: TextStyle(backgroundColor: Colors.grey , 
+           fontStyle: FontStyle.italic, 
+           fontFamily:'Roboto',
+            color: Color.fromRGBO(254, 36, 7, 0.962),
+            fontSize: 30),),
+           
         ),
+        
       ),
     );
   }
