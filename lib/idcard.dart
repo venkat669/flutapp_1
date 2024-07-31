9c5739fdd16eb8465ab4c82062c3a8666fa4d1bb
venkat669 <venkatramanvaithee@gmail.com>
2024-07-31 14:42:32 +0530

    "font of text changed  and images are added , custom fonts are applied , new file idcard created"

---
+++ lib/idcard.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Idcard(),
+  ));
+}
+
+class Idcard extends StatelessWidget {
+  const Idcard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.teal,
+      appBar: AppBar(
+        title: const Text('Title'),
+        backgroundColor: Colors.blue,
+        shadowColor: Colors.grey.withOpacity(0.5),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              child: Column(
+                children: [
+                  CircleAvatar(
+                    radius: 50,
+                    backgroundImage: AssetImage('assets/images/meimg.jpg'),
+                  ),
+                  SizedBox(
+                    height:
+                        19, // creates space between the image and text of size 19
+                  ),
+                  Text(
+                    "Venkatraman Vaithee",
+                    style: TextStyle(
+                      fontFamily: 'MajorMonoDisplay',
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
